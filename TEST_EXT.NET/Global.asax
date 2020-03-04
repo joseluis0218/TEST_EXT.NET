c3690db809a1f939d05a32a282def046bf92ded4
Jose Cristobal <jcristobal@hplperu.com>
2020-03-03 23:51:27 -0500

    Agregar archivos de proyecto.

---
+++ TEST_EXT.NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TEST_EXT.NET.MvcApplication" Language="C#" %>
---
+++ TEST_EXT.NET/Models/ExtNetModel.cs
@@ -0,0 +1,8 @@
+namespace TEST_EXT.NET.Models
+{
+    public class ExtNetModel
+    {
+        public string Title { get; set; }
+        public string TextAreaEmptyText { get; set; }
+    }
+}
